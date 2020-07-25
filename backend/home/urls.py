80ca679e04b55a4156ec353515d574a6f4aa32ea
Swaraj Kaondal <swarajkaon@gmail.com>
2020-07-25 17:45:33 +0530

    Question api done

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('Questions/',views.questions, name = 'questions'),
+    path('Feedback/',views.questions, name = 'questions'),
+    path('questionEntry/',views.loadQuestions, name = 'loadQuestions'),
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Question,Feedback,User
 import json
-import pandas as pd
+from .databaseEntry import addQuestions
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
 def questions(request):
     jsonResponse = {}
     questionList = []
@@ -19,4 +20,31 @@ def questions(request):
     
     jsonResponse['Questions'] = questionList
     return JsonResponse(jsonResponse)
+
+@csrf_exempt 
+def feedback(request):
     
+
+@csrf_exempt    
+def loadQuestions(request):
+    if request.method != "POST":
+        return JsonResponse({"data":"use post"})
+
+    questions = addQuestions()
+    for question in questions:
+        temp = Question(id = question["id"],question = question["Question"])
+        temp.save()
+
+    return JsonResponse({"success":"True"})
+
+@csrf_exempt    
+def loadFeedback(request):
+    if request.method != "POST":
+        return JsonResponse({"data":"use post"})
+
+    feedbacks = addFeedbacks()
+    for feedback in feedbacks:
+        temp = Feedback(id = question["id"],question = question["Question"])
+        temp.save()
+
+    return JsonResponse({"success":"True"})
