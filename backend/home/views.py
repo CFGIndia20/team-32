cd73a519cf71ff7be2f3f1f4b490cb3fc1d6adb4
Anurag Madnawat <anuragmadnawat1@gmail.com>
2020-07-25 17:48:30 +0530

    added api support

--- backend/home/views.py
+++ backend/home/views.py
@@ -4,6 +4,9 @@ from .models import Question,Feedback,User
 import json
 import pandas as pd
 from django.views.decorators.csrf import csrf_exempt
+from google.cloud import translate_v2
+from gtts import gTTS
+import soundfile
 
 # Create your views here.
 def questions(request):
@@ -19,4 +22,46 @@ def questions(request):
     
     jsonResponse['Questions'] = questionList
     return JsonResponse(jsonResponse)
-    
+
+def translate(sourceText, targetLanguage):
+    client = translate_v2.Client()
+    response = client.translate(sourceText,target_language=target_language)
+    return response["translatedText"]
+
+def textToSpeech(sourceText, sourceLanguage):
+    obj = gTTS(text=sourceText, slow= False, lang=sourceLanguage)
+    return obj
+
+def speechToText(local_file_path, language):
+    audio_samples, sample_rate = soundfile.read(local_file_path, dtype='int16')
+    	print(sample_rate)
+    	client = speech_v1.SpeechClient()
+
+    	# local_file_path = 'resources/brooklyn_bridge.raw'
+
+    	# The language of the supplied audio
+    	language_code = language
+
+    	# Sample rate in Hertz of the audio data sent
+    	sample_rate_hertz = sample_rate
+    	# Encoding of audio data sent. This sample sets this explicitly.
+    	# This field is optional for FLAC and WAV audio formats.
+    	encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    	config = {
+    		"language_code": language_code,
+    		"sample_rate_hertz": sample_rate_hertz,
+    		"encoding": encoding,
+    	}
+    	with io.open(local_file_path, "rb") as f:
+    		content = f.read()
+    	audio = {"content": content}
+
+    	response = client.recognize(config, audio)
+    	print(response)
+    	resultString="";
+    	for result in response.results:
+    		# First alternative is the most probable result
+    		alternative = result.alternatives[0]
+    		resultString = resultString + format(alternative.transcript)
+
+    	return resultString
